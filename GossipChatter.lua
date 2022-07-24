4554407f466e06bdbfc5e8e5c2d1788c2e486512
keyboardturner <20036792+keyboardturner@users.noreply.github.com>
2022-07-23 20:24:53 -0700

    addon files
    addon files

---
+++ GossipChatter.lua
@@ -0,0 +1,38 @@
+local GossipTracker = CreateFrame("Frame");
+GossipTracker:RegisterEvent("GOSSIP_SHOW");
+GossipTracker:RegisterEvent("QUEST_PROGRESS");
+GossipTracker:RegisterEvent("QUEST_COMPLETE");
+GossipTracker:RegisterEvent("QUEST_DETAIL");
+function GossipTracker:OnEvent(event,arg1)
+	local info = ChatTypeInfo["MONSTER_SAY"]
+	if event == "GOSSIP_SHOW" then
+		local sender = GossipFrameNpcNameText:GetText()
+		local body = CHAT_SAY_GET:format(sender) .. GossipGreetingText:GetText()
+		
+		DEFAULT_CHAT_FRAME:AddMessage(body, info.r, info.g, info.b, info.id)
+	end
+	if event == "QUEST_PROGRESS" then
+		local sender = QuestFrameNpcNameText:GetText()
+		local body = CHAT_SAY_GET:format(sender) .. QuestProgressText:GetText()
+		
+		DEFAULT_CHAT_FRAME:AddMessage(body, info.r, info.g, info.b, info.id)
+	end
+	if event == "QUEST_COMPLETE" then
+		local sender = QuestFrameNpcNameText:GetText()
+		local body = CHAT_SAY_GET:format(sender) .. QuestInfoRewardText:GetText()
+		
+		DEFAULT_CHAT_FRAME:AddMessage(body, info.r, info.g, info.b, info.id)
+	end
+	if event == "QUEST_DETAIL" then
+		local sender = QuestFrameNpcNameText:GetText()
+		local body = CHAT_SAY_GET:format(sender) .. QuestInfoDescriptionText:GetText()
+		
+		DEFAULT_CHAT_FRAME:AddMessage(body, info.r, info.g, info.b, info.id)
+	end
+end
+GossipTracker:SetScript("OnEvent",GossipTracker.OnEvent);
+--[[
+	local info = ChatTypeInfo["MONSTER_SAY"]
+	local body = CHAT_SAY_GET:format(sender) .. msg
+	DEFAULT_CHAT_FRAME:AddMessage(body, info.r, info.g, info.b, info.id)
+]]
