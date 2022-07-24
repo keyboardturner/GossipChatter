a7dfb1794eae4ca4a80df7bdca202313eeade6aa
keyboardturner <20036792+keyboardturner@users.noreply.github.com>
2022-07-23 21:03:42 -0700

    added item text
    added item text in the form of creature emote ![example](https://i.imgur.com/rhdjdPm.png)

--- GossipChatter.lua
+++ GossipChatter.lua
@@ -3,6 +3,7 @@ GossipTracker:RegisterEvent("GOSSIP_SHOW");
 GossipTracker:RegisterEvent("QUEST_PROGRESS");
 GossipTracker:RegisterEvent("QUEST_COMPLETE");
 GossipTracker:RegisterEvent("QUEST_DETAIL");
+GossipTracker:RegisterEvent("ITEM_TEXT_READY");
 function GossipTracker:OnEvent(event,arg1)
 	local info = ChatTypeInfo["MONSTER_SAY"]
 	if event == "GOSSIP_SHOW" then
@@ -29,10 +30,28 @@ function GossipTracker:OnEvent(event,arg1)
 		
 		DEFAULT_CHAT_FRAME:AddMessage(body, info.r, info.g, info.b, info.id)
 	end
+	if event == "ITEM_TEXT_READY" then	
+		local info = ChatTypeInfo["MONSTER_EMOTE"]
+		local sender = ItemTextFrameTitleText:GetText()
+		local body = CHAT_EMOTE_GET:format(sender) .. "Page " .. ItemTextCurrentPage:GetText() .. ": " .. ItemTextGetText()
+		
+		DEFAULT_CHAT_FRAME:AddMessage(body, info.r, info.g, info.b, info.id)
+	end
 end
 GossipTracker:SetScript("OnEvent",GossipTracker.OnEvent);
 --[[
 	local info = ChatTypeInfo["MONSTER_SAY"]
 	local body = CHAT_SAY_GET:format(sender) .. msg
 	DEFAULT_CHAT_FRAME:AddMessage(body, info.r, info.g, info.b, info.id)
+
+	GossipFrameNpcNameText
+	GossipGreetingText
+
+	QuestFrameNpcNameText
+	QuestInfoDescriptionText
+	QuestProgressText
+	QuestInfoRewardText
+
+	ItemTextFrameTitleText
+
 ]]
